2d010521b17cd49f97f31b4e4146dc43915c8c4a
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2020-05-20 16:05:48 +0530

    Initial commit.

---
+++ src/load-magic-link.js
@@ -0,0 +1,47 @@
+import EventEmitter from 'event-emitter';
+
+const events = new EventEmitter();
+let MagicSDK = null;
+let script = null;
+let sdk = null;
+
+export default async function loadMagicLink(key) {
+    if (sdk) {
+        return sdk;
+    }
+
+    if (key && MagicSDK) {
+        sdk = new MagicSDK(key)
+        return sdk;
+    }
+
+    if (!script) {
+        console.log('Loading Magic');
+        script = window.document.createElement('script');
+        script.async = true;
+        script.src = 'https://cdn.jsdelivr.net/npm/magic-sdk/dist/magic.js';
+        script.id = 'magic-link-sdk';
+        script.onload = () => {
+            MagicSDK = window.Magic;
+            events.emit('loaded');
+        }
+        
+        document.head.appendChild(script);
+    }
+
+    if (!key) {
+        return
+    }
+
+    return new Promise((resolve) => {
+        events.once('loaded', () => {
+            if (sdk) {
+                resolve(sdk)
+                return;
+            }
+            
+            sdk = new MagicSDK(key);
+            resolve(sdk);
+        })
+    })
+}
---
+++ src/use-magic-link.js
@@ -0,0 +1,150 @@
+import { useState, useEffect } from 'react'
+import EventEmitter from 'event-emitter'
+import unfetch from 'isomorphic-unfetch'
+import ms from 'ms'
+import { Sema } from 'async-sema'
+import loadMagicLink from './load-magic-link'
+
+const tokenSema = new Sema(1)
+const loggedInSema = new Sema(1)
+const loginEvents = new EventEmitter()
+
+let currentLoginState = null
+let currentToken = null
+
+if (typeof window !== 'undefined') {
+  loadMagicLink()
+}
+
+async function getMagicToken (magicLinkKey) {
+  await tokenSema.acquire()
+  try {
+    if (currentToken && currentToken.expiredAt > Date.now()) {
+      return currentToken.token
+    }
+
+    const magic = await loadMagicLink(magicLinkKey);
+    const token = await magic.user.getIdToken()
+    setToken(token)
+    return token
+  } finally {
+    tokenSema.release()
+  }
+}
+
+async function isLoggedIn (magicLinkKey) {
+  await loggedInSema.acquire()
+
+  try {
+    if (currentLoginState !== null) {
+      return currentLoginState
+    }
+
+    await getMagicToken(magicLinkKey)
+    currentLoginState = true
+  } catch (err) {
+    currentLoginState = false
+  } finally {
+    loggedInSema.release()
+  }
+
+  return currentLoginState
+}
+
+function setToken (token, lifespan = ms('15min')) {
+  currentToken = {
+    token,
+    expiredAt: Date.now() + lifespan - ms('1min')
+  }
+}
+
+export default function useAuth (magicLinkKey) {
+  if (!magicLinkKey) {
+    throw new Error('Magic Link publishableKey required as the first argument')  
+  }
+
+  const [loggedIn, setLoggedIn] = useState(currentLoginState !== null ? currentLoginState : false)
+  const [loading, setLoading] = useState(currentLoginState === null)
+  const [error, setError] = useState(null)
+  const [loggingIn, setLoggingIn] = useState(false)
+  const [loggingOut, setLoggingOut] = useState(false)
+
+  async function login (email) {
+    setError(null)
+    setLoggingIn(true)
+
+    try {
+      const magic = await loadMagicLink(magicLinkKey);
+      const token = magic.auth.loginWithMagicLink({ email })
+      currentLoginState = true
+      setToken(token)
+      loginEvents.emit('loggedIn', true)
+      setLoggedIn(true)
+    } catch(err) {
+      setError(err);
+    } finally {
+      setLoggingIn(false)
+    }
+  }
+
+  async function logout () {
+    setError(null)
+    setLoggingOut(true)
+
+    try {
+      const magic = await loadMagicLink(magicLinkKey);
+      await magic.user.logout()
+      currentLoginState = null
+      currentToken = null
+      loginEvents.emit('loggedIn', false)
+      setLoggedIn(false)
+    } catch (err) {
+      setError(err)
+    } finally {
+      setLoggingOut(false)
+    }
+
+    return currentLoginState === null
+  }
+
+  async function fetch (url, opts = {}) {
+    const token = await getMagicToken(magicLinkKey)
+    if (token) {
+      opts.headers = opts.headers || {}
+      opts.headers.Authorization = `Bearer ${token}`
+    }
+
+    return unfetch(url, opts)
+  }
+
+  useEffect(() => {
+    if (!currentLoginState) {
+      isLoggedIn(magicLinkKey)
+        .then((loginState) => {
+          setLoggedIn(loginState)
+        })
+        .then(() => setLoading(false))
+    }
+
+    function watchLoggedIn (state) {
+      setLoggedIn(state)
+    }
+    loginEvents.on('loggedIn', watchLoggedIn)
+
+    return () => {
+      loginEvents.off('loggedIn', watchLoggedIn)
+    }
+  }, [currentLoginState])
+
+  return {
+    loggedIn,
+    loading,
+    error,
+    loggingIn,
+    loggingOut,
+    login,
+    logout,
+    fetch,
+    loginEvents
+  }
+}
